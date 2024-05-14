d9e92728609b814878c3da76bb1b80a8fec3d614
Adam <adam.devlaming@edu.sait.ca>
2024-05-14 12:31:17 -0600

    Step 2 of week 2 assignmen
    Changed root page, created component, and made week-2 page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h2>Adam de Vlaming</h2>
+            <Link href="https://github.com/adevlaming/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    )
+}
